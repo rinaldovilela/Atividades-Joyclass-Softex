45f8c7cc821ecf6d99927c8e0c90e9bc81714db1
Rinaldo <rinaldoalvesvilela@hotmail.com>
2023-11-14 06:56:25 -0300

    📦  upload ATV JavaScript - 1

---
+++ Joyclass - Atividade JavaScript 1/script.js
@@ -0,0 +1,11 @@
+// Definindo as variáveis
+let nome = "João Silva"; // String
+let salario = 50000.50; // Number
+let idade = 25; // Number
+let possuiDiploma = true; // Boolean
+
+// Exibindo os dados no console
+console.log("Nome: " + nome);
+console.log("Salário: " + salario);
+console.log("Idade: " + idade);
+console.log("Possui Diploma: " + possuiDiploma);
