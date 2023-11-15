94d38c89a58f6bab62604a0f367fd7a724dbb41d
Rinaldo <rinaldoalvesvilela@hotmail.com>
2023-11-15 19:35:46 -0300

    ✅  Adicionando atividade javascript  2

---
+++ Joyclass - Atividade JavaScript 2/script.js
@@ -0,0 +1,9 @@
+// Calculadora de média 
+
+let nota1 = parseFloat(prompt("Digite a primeira nota: "));
+let nota2 = parseFloat(prompt("Digite a segunda nota: "));
+let nota3 = parseFloat(prompt("Digite a terceira nota: "));
+
+const media = (nota1 + nota2 + nota3) / 3
+
+console.log(`A média das notas é igual a = ${media}`)
